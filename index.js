d9bca63715ed9e9800e332f24af8428a5653cc12
Bradw02 <airpro.brad@gmail.com>
2022-02-17 03:38:17 -0600

    commit 3 changes

--- index.js
+++ index.js
@@ -2,19 +2,20 @@
 let firstAnswer  = window.prompt('Do you head left or right?')
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
-It scampers off down a small hole, just large enough for you to crawl through. 
-Do you follow it, or continue on your path?`)
-    if (thirdAnswer === 'follow it') {
-        let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
-        if(thirdAnswer === 'stay') {
-        } else if (secondAnswer === 'spread the word') {
-            let thirdAnswer = window.alert('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
-        }
-
-    }
-} else if(firstAnswer === 'right') {
-    let secondAnswer = window.prompt(`You come across a snoring dragon. 
-On the other side of him, you see a shiny chest of treasure. Another path would 
-lead you away from the dragon altogether. Which path do you take?`)
-
-}
+        It scampers off down a small hole, just large enough for you to crawl through.Do you follow it, or stay?`)
+        if (secondAnswer === 'follow it') {
+                let thirdAnswer= windows.prompt('You follow the cat to a colony of cats nestled in a fort of warm blankets and subsisting off inexpicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical saft HashChangeEvent.')}
+                if (secondAnswer === 'stay') {
+                    let thirdAnswer = console.log ('you live happily amoungist the cats for the rest of your days.END Yes or No') }
+                        else if (thirdAnswer === 'Yes') {
+                            let fourthhAnswer = window.prompt('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless. To continue yes or No?')}
+if (anyAnswer === 'Yes') {
+    let windows.prompt('Do you want to know what if you went Right? Yes or No?' )        
+    else if(anyAnswer === 'Yes') {
+        let secondAnswer = window.prompt(`You come across a snoring dragon. 
+        On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Stay or take the path?`)
+        if (thirdAnswer === 'take-the-path'){
+             let thirdfAnswer = window.prompt('You come across a chamber thats reaches upward to shining light above. There is a long, winding stairscase, and much quicker,  rickety-looking ladder that leads up atwards tje light. which do you take stairs or ladder?')
+            if (thirdAnswer === 'stay') {
+                let fourthAnswer =('You and the dragon have an uplifting conversation about local politics and become lifelong friends. END') }
+                }
